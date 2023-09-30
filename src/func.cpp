0371a8be6882b84325cb301b261364864a8df409
Sam Knoop <samknoop@gmail.com>
2023-09-30 14:32:29 +0100

    log on init

--- src/func.cpp
+++ src/func.cpp
@@ -5,13 +5,16 @@ namespace log {
 Log global_log;
 
 using std::string;
-void log(string message, char indent_change) {
-    if (global_log.get_file_name().empty()) init("default.log");
-    global_log.write(message, indent_change);
+void log (string message, char indent_change) {
+    if (global_log.get_file_name ().empty ()) init ("default.log");
+    global_log.write (message, indent_change);
 }
 
-void init(string file_name) { global_log = Log(file_name); }
+void init (string file_name) {
+    global_log = Log (file_name);
+    log ("Logger intialised");
+}
 
-void set_time_format(string format) { global_log.time_format = format; }
-};  // namespace log
-};  // namespace spk
+void set_time_format (string format) { global_log.time_format = format; }
+}; // namespace log
+}; // namespace spk
