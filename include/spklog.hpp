ca64f965c4bc6f003941ee37e27a028ab0ef1585
Sam Knoop <samknoop@gmail.com>
2023-09-30 11:10:44 +0100

    reorder header

--- include/spklog.hpp
+++ include/spklog.hpp
@@ -4,29 +4,31 @@
 
 namespace spk {
 namespace log {
+
+void init(std::string file_name);
+void log(std::string message, char indent_change = 0);
+void set_time_format(std::string format, size_t length);
+
 class Log : public std::ofstream {
-  public:
-    Log ();
-    Log (std::string file);
-    Log (Log& log);
-    ~Log ();
+   public:
+    Log();
+    Log(std::string file);
+    Log(Log& log);
+    ~Log();
 
-    Log& operator= (const Log& log);
+    Log& operator=(const Log& log);
 
-    void write (std::string message, char indent_change);
+    void write(std::string message, char indent_change);
 
-  private:
+   private:
     std::string file_name;
     std::string time_format = "%F %T";
-    short       time_length = 20;
-    short       indent      = 0;
+    short time_length = 20;
+    short indent = 0;
 
-    friend void set_time_format (std::string format, size_t length);
+    friend void set_time_format(std::string format, size_t length);
 };
 
-void       log (std::string message, char indent_change = 0);
-void       init (std::string file_name);
-void       set_time_format (std::string format, size_t length);
 extern Log global_log;
-}; // namespace log
-}; // namespace spk
+};  // namespace log
+};  // namespace spk
